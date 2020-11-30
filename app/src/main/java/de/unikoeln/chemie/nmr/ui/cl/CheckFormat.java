0c75fb1ceecef0e06023e01f68cc3e4ea9f9c1c2
Stefan Kuhn <stefan.kuhn@dmu.ac.uk>
2020-11-30 14:40:08 +0000

    extended output

--- app/src/main/java/de/unikoeln/chemie/nmr/ui/cl/CheckFormat.java
+++ app/src/main/java/de/unikoeln/chemie/nmr/ui/cl/CheckFormat.java
@@ -3,11 +3,15 @@ package de.unikoeln.chemie.nmr.ui.cl;
 import java.io.FileInputStream;
 
 import org.jcamp.spectrum.NMRSpectrum;
+import org.jcamp.spectrum.notes.Note;
+import org.jcamp.spectrum.notes.NoteDescriptor;
 import org.openscience.cdk.interfaces.IMolecularFormula;
 import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
 
+import de.unikoeln.chemie.nmr.data.NMR2DSpectrum;
 import de.unikoeln.chemie.nmr.data.NmreData;
 import de.unikoeln.chemie.nmr.io.NmredataReader;
+import javafx.scene.control.Tab;
 
 public class CheckFormat {
 
@@ -22,8 +26,14 @@ public class CheckFormat {
         System.out.println("The molecule in your file has formula "+MolecularFormulaManipulator.getString(mfa));
 		System.out.println("Your file contains "+data.getSpectra().size()+" spectra");
 		for(int i=0; i<data.getSpectra().size(); i++){
-			if(data.getSpectra().get(i) instanceof NMRSpectrum)
-				System.out.println("Spectrum "+i+" has "+((NMRSpectrum)data.getSpectra().get(i)).getPeakTable().length+" peaks");
+			if(data.getSpectra().get(i) instanceof NMRSpectrum) {
+		        NoteDescriptor noteDescriptor=new NoteDescriptor("CorType");
+				System.out.println("1D Spectrum "+i+" is a "+(data.getSpectra().get(i).getNotes(noteDescriptor)==null ? "unknown type" : ((Note)data.getSpectra().get(i).getNotes(noteDescriptor).get(0)).getValue())+" spectrum and has "+((NMRSpectrum)data.getSpectra().get(i)).getPeakTable().length+" peaks");
+			}
+			if(data.getSpectra().get(i) instanceof NMR2DSpectrum) {
+		        NoteDescriptor noteDescriptor=new NoteDescriptor("CorType");
+				System.out.println("2D Spectrum "+i+" is a "+(data.getSpectra().get(i).getNotes(noteDescriptor)==null ? "unknown type" : ((Note)data.getSpectra().get(i).getNotes(noteDescriptor).get(0)).getValue())+" spectrum and has "+((NMR2DSpectrum)data.getSpectra().get(i)).getPeakTable().length+" peaks");
+			}
 		}
 	}
 
